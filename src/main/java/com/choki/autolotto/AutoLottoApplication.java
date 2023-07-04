a6022c65befa04a5bf273fa1d1f6a8e8ce259cd9
jiseok-Choi <choi300300@gmail.com>
2023-07-05 00:09:18 +0900

    initialize project

---
+++ src/main/java/com/choki/autolotto/AutoLottoApplication.java
@@ -0,0 +1,13 @@
+package com.choki.autolotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLottoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLottoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choki/autolotto/AutoLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choki.autolotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoLottoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
